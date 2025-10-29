2f49feaf990f4ccba37e6bf8a83dc1ddedc00ae3
Jet Pham <jetthomaspham@gmail.com>
2025-10-29 16:37:13 -0700

    feat: add - for loading in registered count

--- src/app/_components/registered-count.tsx
+++ src/app/_components/registered-count.tsx
@@ -3,12 +3,12 @@
 import { api } from "~/trpc/react";
 
 export function RegisteredCount() {
-  const { data: stats } = api.stats.getRegisteredCount.useQuery();
+  const { data: stats, isLoading } = api.stats.getRegisteredCount.useQuery();
 
   return (
     <div className="text-center text-xl">
       <span className="font-bold" style={{ color: "var(--color-compsigh)" }}>
-        {stats?.count ?? 0}
+        {isLoading ? "-" : stats?.count ?? 0}
       </span>{" "}
       {stats?.count === 1 ? "Person Registered" : "People Registered"}
     </div>
